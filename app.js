2259475bf8d4ce65d8cce2690268fe846249cb85
mario-mitchell <mario.mitchell8291@gmail.com>
2018-12-29 20:01:09 -0800

    work in progress

---
+++ app.js
@@ -0,0 +1,24 @@
+let name =  "Name: Mario Mitchell";
+let career = "Career: Wanna Be Fullstack Developer";
+let aboutMe = "About: I want to be a cool developer. I want to leave a mark in the world. I'm a military veteran that attends Covalence right now.";
+let interests= [
+    "Cooking", "Gaming", "Gym", "Spendning Time with my Family"
+]
+let previousExperience = [
+    "United States Air Force for 9 years", "Sales and Marketing", "Currently doing IT for William Hill Sports Booking", "Covalence School by Night"
+]
+
+function displayPosition(){
+    console.log(name);
+    console.log(career);
+    console.log(aboutMe);
+    console.log(interests);
+    console.log(previousExperience);
+}
+
+function displaySkills(){
+    console.log("BAM: Medic");
+}
+
+displayPosition();
+displaySkills();
